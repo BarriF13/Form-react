9b7f7f17c08a8c10350dea5e3a5b97b8a175256e
Barri <info@bdesignstudio.co.uk>
2020-07-10 18:39:14 +0100

    Use of controlled and uncontrolled comp

--- src/components/controlled.js
+++ src/components/controlled.js
@@ -3,13 +3,41 @@ import React, { Component } from 'react';
 class Controlled extends Component {
 
     state = {
-        
+        name: '',
+        lastName: ''
+    }
+    handleNameChange =(e)=>{
+       this.setState({
+           name:e.target.value
+       })
+    }
+    handleLastNameChange=(e)=>{
+        this.setState({
+            lastName:e.target.value
+        })
     }
 
-    render(){
-        return(
-            <div>
-                Controlled
+    render() {
+        return (
+            <div className="container">
+                <form action="">
+                    <div className="form_element">
+                        <label htmlFor="">Enter name</label>
+                        <input type="text"
+                        onChange={this.handleNameChange}
+                            value={this.state.name}
+                        />
+                    </div>
+
+                    <div className="form_element">
+                        <label htmlFor="">Enter Lastname</label>
+                        <input type="text"
+                        onChange={this.handleLastNameChange}
+                        value={this.state.lastName}
+
+                        />
+                    </div>
+                </form>
             </div>
         )
     }
--- src/components/uncontrolled.js
+++ src/components/uncontrolled.js
@@ -2,14 +2,38 @@ import React, { Component } from 'react';
 
 class Uncontrolled extends Component {
 
-    state = {
+   
+    handleSubmit = (e) => {
+        e.preventDefault();
 
+        const values = {
+            name: this.name.value,
+            lastName: this.lastName.value
+        }
+        console.log(values);
     }
+   
+    render() {
+        return (
+            <div className="container">
+                <form >
+                    <div className="form_element">
+                        <label htmlFor="">Enter name</label>
+                        <input
+                         type="text"
+                         ref={input => this.name = input}
+                    />
+                    </div>
 
-    render(){
-        return(
-            <div>
-                Uncontrolled
+                    <div className="form_element">
+                        <label htmlFor="">Enter Lastname</label>
+                        <input 
+                        type="text"
+                        ref={input => this.lastName = input}
+                    />
+                    </div>
+                    <button onClick={this.handleSubmit}>Sign in</button>
+                </form>
             </div>
         )
     }
