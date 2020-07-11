4d0a45129ee0ee066460e948789d35e1fad48237
Barri <info@bdesignstudio.co.uk>
2020-07-11 17:22:56 +0100

    Validate

--- src/components/user.js
+++ src/components/user.js
@@ -13,7 +13,14 @@ class User extends Component {
                     name:'name_input',
                     text:'text',
                     placeholder:  'Enter your name'
-                }
+                },
+                validation:{
+                    required:true,
+                    minLen:5
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
             },
             lastName:{
                 element:'input',
@@ -23,23 +30,91 @@ class User extends Component {
                 config:{
                     name:'name_input',
                     type:'text',
-                    placeholder:  'Enter your  Last name'
-                }
+                    placeholder: 'Enter your  Last name'
+                },
+                validation:{
+                    required:true,
+                    
+                },
+                valid:false,
+                touched:false,
+                validationMessage:''
+            },
+            message:{
+                element:'textarea',
+                value:'',
+                label:true,
+                labelText:'write your msg',
+                config:{
+                    name:'message_input',
+                    rows:4,
+                    cols:36
+                },
+                validation:{
+                    required:false,
+                    
+                },
+                valid:true
+               
+            },
+            age:{
+                element:'select',
+                value:'',
+                label:true,
+                labelText:'Age',
+                config:{
+                    name:'age_input',
+                    option:[
+                        { val:'1', text:'10-20'},
+                        { val:'2', text:'20-30'},
+                        { val:'3', text:'+30'},
+                    ]
+                },
+                validation:{
+                    required:false,
+                    
+                },
+                valid:true
             }
         }
     }
+    updateForm = (newState) => {
+        this.setState({
+            formData:newState
+        })
+    }
+    submitForm =(e) =>{
+        e.preventDefault();
+        let dataToSubmit = {};
+        let formIsValid = true;
 
+        for(let key in this.state.formData){
+            dataToSubmit[key] = this.state.formData[key].value;
+        }
+        for(let key in this.state.formData){
+          formIsValid = this.state.formData[key].valid && formIsValid; 
+        }
+
+        if(formIsValid){
+            console.log(dataToSubmit);
+        }
+   
+        // console.log(dataToSubmit);
+        //now data is ready to submit anywhere
+        //for example we can do :
+        //axios.post(url, dataToSubmit)
+    }
     render(){
         return(
             <div className="container">
-              <form onSubmit="{this.submitForm}">
+              <form onSubmit={this.submitForm}>
 
                 <FormFields
                     formData={this.state.formData}
+                    onBlur={(newState)=> this.updateForm(newState)}
+                    change={(newState)=> this.updateForm(newState)}
                 />
-
-
-                  <button type="submit">Submit</button>
+                <button type="submit">Submit</button>
               </form>
             </div>
         )
--- src/widgets/forms/formFields.js
+++ src/widgets/forms/formFields.js
@@ -1,63 +1,160 @@
 import React from 'react'
-
+//we are restucturing all the data from form to render it 
 const FormFields = (props) => {
-
+  //getting data the data ---------------------------
   const renderFields = () => {
-
+    //turn on formData to array
     const formArray = [];
-    for(let elementName in props.formData) {
+    for (let elementName in props.formData) {
       formArray.push({
-        id:elementName,
-        settings:props.formData[elementName]
+        id: elementName,
+        settings: props.formData[elementName]
       })
     }
     // console.log(formArray);
     //returning each element sorted for viewing
-    return formArray.map( (item, i)=>{
+    return formArray.map((item, i) => {
       return (
         <div key={i} className="form_element">
           {/* making a method to check elements type --item is all the info in name: for example */}
           {renderTemplates(item)}
-        
+
         </div>
       )
     })
 
   }
   //checks if we have a label or not
-  const showLabel = (show, label) =>{
-    return show ? 
-  <label >{label}</label>
-    : null;
+  const showLabel = (show, label) => {
+    return show ?
+      <label >{label}</label>
+      : null;
   }
+
+  //changeHandler event----------------------
+  const changeHandler = (e, id, blur) => {
+    const newState = props.formData;
+    newState[id].value = e.target.value;
+    //use blur to stop validation while typing the word
+    if (blur) {
+      //form validation--valid func receiving data to check
+      let validData = validate(newState[id])
+      newState[id].valid = validData[0];
+      newState[id].validationMessage = validData[1];
+    }
+    newState[id].touched = blur;
+
+    props.change(newState)
+  }
+  const validate = (element) => {
+    console.log(element);
+    let error = [true, ''];
+
+    //rule two min length
+    if (element.validation.minLen) {
+      const valid = element.value.length >= element.validation.minLen;
+      const message = `${!valid ? 'Must be greater than' + element.validation.minLen : ''}`
+
+      error = !valid ? [valid, message] : error
+    }
+
+
+    //rule one if there is any letter
+    if (element.validation.required) {
+      const valid = element.value.trim() !== '';
+      const message = `${!valid ? 'This field is required' : ''}`
+
+      error = !valid ? [valid, message] : error
+    }
+    return error;
+  }
+  // show validation
+  const showValidation = (data) => {
+    let errorMessage = null;
+    if (data.validation && !data.valid) {
+      errorMessage = (
+        <div className="label_error">
+          {data.validationMessage}
+        </div>
+      )
+    }
+    return errorMessage;
+  }
+
   //making the render template function
   const renderTemplates = (data) => {
     let formTemplate = '';
     let values = data.settings; //shortening= data.setting.config and bla 
 
     //we are creating a switch to check data.setting.element -(data.setting.'input'?)
-    switch(values.element) {
-      case('input'):
-          formTemplate = (
-            <div>
-              {showLabel(values.label, values.labelText)}
-              <input
-                 {...values.config}
-                 value={values.value}
-              />
-            </div>
-          )
+    switch (values.element) {
+      case ('input'):
+        formTemplate = (
+          <div>
+            {showLabel(values.label, values.labelText)}
+            <input
+              {...values.config}
+              value={values.value}
+              onBlur={
+                (e) => changeHandler(e, data.id, true)
+              }
+              onChange={
+                (e) => changeHandler(e, data.id)
+              }
+            />
+            {showValidation(values)}
+          </div>
+        )
         break;
-        default:
-          formTemplate =null
+      case ('textarea'):
+        formTemplate = (
+          <div>
+            {showLabel(values.label, values.labelText)}
+            <textarea
+              {...values.config}
+              value={values.value}
+            
+              onChange={
+                (e) => changeHandler(e, data.id, false)
+              }
+            />
+
+          </div>
+        )
+        break;
+      case ('select'):
+        formTemplate = (
+          <div>
+            {showLabel(values.label, values.labelText)}
+            <select
+              value={values.value}
+              name={values.config.name}
+              onChange={
+                (e) => changeHandler(e, data.id)
+              }
+            >
+              {values.config.option.map((item, i) => {
+                return (
+                  <option key={i} value={item.val}>
+                    {item.text}
+                  </option>
+                )
+              })}
+            </select>
+
+          </div>
+        )
+        break;
+      default:
+        formTemplate = null
     }
     return formTemplate;
   }
 
   return (
     <div>
-      {renderFields()} 
-      
+      {renderFields()}
+
     </div>
   )
 }
